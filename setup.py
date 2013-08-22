4ccd3f5c5659d182bf9dfdd6fd12a3a0650fbb5b
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-08-22 13:26:47 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='fidetournament',
+      version=version,
+      description="FIDE Tournament Report File (Krause format) parser and creator",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='chess fide parser',
+      author='Mikel Larreategi',
+      author_email='larreategi@eibar.org',
+      url='https://github.com/erral/fidetournament',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
